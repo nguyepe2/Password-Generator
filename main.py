d599900c457e02635bf41442f9c83998bdabb18d
nguyepe2 <32175436+nguyepe2@users.noreply.github.com>
2021-07-12 19:55:31 -0700

    Made the password generation process more deterministic and gave the user more control over the password

--- main.py
+++ main.py
@@ -11,51 +11,92 @@ def password_generator(name):
 def get_user_input():
     # get the requirements of the password
     length = int(input("How long should the password be?\n"))
-    numbers = int(input("At least how many numbers should there be?\n"))
-    symbols = int(input("At least how many symbols should there be?\n"))
+    numbers = int(input("How many numbers should there be?\n"))
+    symbols = int(input("How many symbols should there be?\n"))
+    caps = int(input("How many capital letters should there be?\n"))
+    lowers = length - numbers - symbols - caps
 
-    requirements = [length, numbers, symbols]
+    requirements = [length, numbers, symbols, caps, lowers]
     return requirements
 
 
 def generate_password(requirements):
     # create the list of usable characters for the password
     alphabet = string.ascii_letters
+    lower_case = string.ascii_lowercase
+    upper_case = string.ascii_uppercase
     digits = list(range(0, 10))
     # cast the digits as str so they can be concatenated to the password
     digit_string = [str(int) for int in digits]
     symbol_list = ['!', '@', '#', '$', '%', '^', '&', '*']
-    valid_characters = list(alphabet) + digit_string + symbol_list
 
-    # generate the password
+    # generate the characters that will be used in the password
+    valid_characters = get_lowercase(requirements[4], lower_case) + get_uppercase(requirements[3], upper_case) + \
+        get_digits(requirements[1], digit_string) + get_symbols(requirements[2], symbol_list)
 
-    # initialize the running counts used to verify that the password requirements are met
-    number_count = 0
-    symbol_count = 0
+    # initialize the password
+    password = ""
 
-    while number_count < requirements[1] or symbol_count < requirements[2]:
-        # initialize the password
-        password = ""
-
-        # get a string of characters as long as the length the user specified
-        for i in range(requirements[0]):
-            # randomly select chars to make the password
-            char = random.randint(0, len(valid_characters) - 1)
-            password = password + valid_characters[char]
-
-        # if a number of symbol exists in the password, increment the respective count
-        for i in password:
-            if i in digit_string:
-                number_count += 1
-            if i in symbol_list:
-                symbol_count += 1
-
-        # if the password doesn't meet the requirements, reset the counts to 0 and try again
-        if number_count < requirements[1] or symbol_count < requirements[2]:
-            number_count = 0
-            symbol_count = 0
+    # generate the password by rearranging the order of the valid_characters
+    for i in range(requirements[0]):
+        pos = random.randint(0, len(valid_characters) - 1)
+        password = password + valid_characters[pos]
+        valid_characters.pop(pos)
+    print(password)
 
     return password
+
+
+# get the uppercase letters for the password
+def get_uppercase(caps, upper_case):
+    # initialize a list to store the uppercase letters
+    selected_upper_case = []
+
+    # randomly select uppercase letters to use in the password
+    for i in range(caps):
+        char = random.randint(0, len(upper_case) - 1)
+        selected_upper_case.append(upper_case[char])
+
+    return selected_upper_case
+
+
+# get the digits for the password
+def get_digits(numbers, digit_string):
+    # initialize a list to store the uppercase letters
+    selected_digits = []
+
+    # randomly select uppercase letters to use in the password
+    for i in range(numbers):
+        char = random.randint(0, len(digit_string) - 1)
+        selected_digits.append(digit_string[char])
+
+    return selected_digits
+
+
+# get the symbols for the password
+def get_symbols(symbols, symbol_list):
+    # initialize a list to store the uppercase letters
+    selected_symbols = []
+
+    # randomly select uppercase letters to use in the password
+    for i in range(symbols):
+        char = random.randint(0, len(symbol_list) - 1)
+        selected_symbols.append(symbol_list[char])
+
+    return selected_symbols
+
+
+# get the lowercase letters for the password
+def get_lowercase(lowers, lower_case):
+    # initialize a list to store the uppercase letters
+    selected_lower_case = []
+
+    # randomly select uppercase letters to use in the password
+    for i in range(lowers):
+        char = random.randint(0, len(lower_case) - 1)
+        selected_lower_case.append(lower_case[char])
+
+    return selected_lower_case
 
 
 # create or append the password to a file
@@ -64,6 +105,6 @@ def output_password(password):
     fo.write(password + '\n')
     fo.close()
 
+
 if __name__ == '__main__':
     password_generator('PyCharm')
-
